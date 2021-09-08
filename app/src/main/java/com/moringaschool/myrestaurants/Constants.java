7eacf5b24aa2c6ab34636198c4f356ba8382177f
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-08 07:44:30 +0300

    adding retrofit to myrestaurant app

--- app/src/main/java/com/moringaschool/myrestaurants/Constants.java
+++ app/src/main/java/com/moringaschool/myrestaurants/Constants.java
@@ -1,6 +1,5 @@
 package com.moringaschool.myrestaurants;
 
-import static com.moringaschool.myrestaurants.BuildConfig.*;
 
 public class Constants {
     public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
